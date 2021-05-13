a6e877d2b6ad1f18f8e03eef129c7c9d97e35b12
VVolkov833 <finnish.ru@gmail.com>
2021-05-13 15:37:10 +0300

    small improvements for the uploaded version

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
 		<meta name="format-detection" content="telephone=no">
 		<?php wp_head(); ?>
 	</head>
