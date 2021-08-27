e1664d9ae8ac055f4a96ba07db369999e387e915
VVolkov833 <finnish.ru@gmail.com>
2021-08-28 00:50:10 +0300

    vertical gallery with scrolling is added

--- assets/common.js
+++ assets/common.js
@@ -12,15 +12,14 @@
         var $ = window.jQuery,
             scrolled = 0;
 
-        /* tracking events */
-//        $( document ).on( 'ready', function() {
-            anchor_links(); // done to mind the top bar, hidding the targeted headline
-            menu_events();
-//        });
-//        $( window ).on( 'load', function() {
-            scrolled = $( window ).scrollTop();
-            body_add_scrolled();
-//        });
+        anchor_links();
+        menu_events();
+
+        scrolled = $( window ).scrollTop();
+        body_add_scrolled();
+
+        addGallery( '.fct-vertical-gallery' ); // ++ move to clinics template
+        
         $( window ).on( 'scroll', function() {
             scrolled = $( window ).scrollTop();
             body_add_scrolled();
@@ -111,16 +110,88 @@
             $( this ).parent().toggleClass( 'fct-active' );
         });
         
-        /* vertical gallery navigation */
-        $( '.fct-vertical-gallery' ).click( function() {
-            var $self = $( this ),
-                $parent = $self.parent(),
-                vpheight = Math.max( document.documentElement.clientHeight || 0, window.innerHeight || 0 );
-            if ( $parent.innerHeight() > $self.outerHeight() ) { return; }
-            var difference = $self.outerHeight() - $parent.innerHeight();
-            $self.css( 'transform', 'translate( 0, -' + difference + 'px )' );
-            // ^^this is just testing ++ add transition
-        });
-        
+        function addGallery(selector) {
+            var $self = $( '.fct-vertical-gallery' ),
+                $children = $self.children();
+
+            // navigation buttons
+            $self.before( '<div class="fct-vertical-gallery-up"></div>' );
+            $self.after( '<div class="fct-vertical-gallery-down"></div>' );
+            var $up = $self.prev( '.fct-vertical-gallery-up' ),
+                $down = $self.next( '.fct-vertical-gallery-down' );
+            
+            $down.click( function() {
+                var visible_height = Math.round( $self.parent().innerHeight() - $self.position()['top'] );
+
+                if ( visible_height > $self.outerHeight() ) { return; }
+
+                var cheights = [],
+                    vpheight = Math.max( document.documentElement.clientHeight || 0, window.innerHeight || 0 ),
+                    offset_now = Math.round( Number( $self.attr( 'data-offset' ) ? $self.attr( 'data-offset' ) : 0 ) ),
+                    offset_max_step = Math.min.apply( null, [
+                        Math.round( visible_height * 3 / 4 ),
+                        Math.round( vpheight * 3 / 4 )
+                    ]),
+                    new_step = 0,
+                    cheight_above = visible_height + offset_now;
+                
+                $children.each( function(a, b) { cheights[a] = Math.floor( $( b ).outerHeight( true ) ); } );
+                
+                // count new offset
+                for ( let i = 0, a = 0, add = 0; i < cheights.length; i++ ) {
+                    a = cheights[i];
+                    add += a;
+
+                    if ( add <= cheight_above ) { continue; }
+
+                    new_step = add - cheight_above;
+
+                    if ( new_step > offset_max_step && new_step !== a ) {
+                        new_step = new_step - a;
+                        break;
+                    }
+                    
+                    if ( i === cheights.length - 1 ) {
+                        $down.hide();
+                    }
+                }
+                $up.show();
+
+                if ( !new_step ) { return; }
+
+                addGallery.process( offset_now + new_step );
+
+            });
+
+            $up.click( function() {
+
+                addGallery.process( 0 );
+                
+                $up.hide();
+                $down.show();
+            });
+            
+            // the motion process
+            addGallery.process = function(offset_new) {
+                $children.each( function( a, b ) {
+                    $( b ).css( 'transform', 'translate( 0, -' + offset_new + 'px )' );
+                });
+                $self.attr( 'data-offset', offset_new );
+            };
+ 
+            // navigation buttons show-hide
+            addGallery.reset = function() {
+                addGallery.process( 0 );
+                
+                $up.hide();
+                $down.hide();
+                if ( $self.parent().innerHeight() - $self.position()['top'] > $self.outerHeight() ) { return; }
+                $down.show();
+            };
+            addGallery.reset();
+            $( window ).resize( function() { addGallery.reset(); });
+
+        }
+       
     });
 } )();
