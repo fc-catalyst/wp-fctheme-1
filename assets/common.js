e230a0c86881b0ff9e01c575c559b6491189903c
VVolkov833 <finnish.ru@gmail.com>
2022-01-19 23:16:36 +0300

    pick can now only follow the view

--- assets/common.js
+++ assets/common.js
@@ -107,63 +107,16 @@
         }
         
         /* add map by class */
-        const gmap_holder = $( '.fct-gmap-view' );
-        if ( gmap_holder.length ) {
+        const $gmap_holder = $( '.fct-gmap-view' );
+        if ( $gmap_holder.length ) {
             fcLoadScriptVariable(
-                'https://maps.googleapis.com/maps/api/js?key='+fcGmapKey+'&libraries=places',
+                'https://maps.googleapis.com/maps/api/js?key='+fcGmapKey+'&libraries=places', // ++unite global variables
                 'google'
             );
             fcLoadScriptVariable(
                 '/wp-content/themes/fct1/assets/smarts/gmap-view.js?' + + new Date(),
                 'fcAddGmapView',
-                function() {
-                    const gmap = fcAddGmapView( gmap_holder ); // ++move the following to gmap-pick.js and include after view (single marker mode, or something)
-
-                    if ( !gmap ) { return }
-                    
-                    // add draggable marker
-                    const marker = new google.maps.Marker( {
-                            position: gmap.getCenter(),
-                            gmap,
-                            icon: {
-                                url: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 25 36'%3E%3Cpath d='M 12.5,-2e-7 C 5,-2e-7 0,5.5 0,12.4 0,19.9 12.6,36 12.6,36 c 0,0.25 12.4,-16.1 12.4,-23.6 C 25,5.5 19,-2e-7 12.5,-2e-7 Z m 0.1,5.3 a 7,7 0 0 1 7,7 7,7 0 0 1 -7,7 7,7 0 0 1 -7,-7 7,7 0 0 1 7,-7 z' fill='%2323667b' stroke='none'/%3E%3C/svg%3E",
-                                scaledSize: new google.maps.Size(25, 36)
-                            },
-                            title: 'Drag to better specify the location',
-                            draggable: true,
-                            animation: google.maps.Animation.DROP
-                        });
-
-                        marker.setMap( gmap );
-
-                    // trigger the 'map_changed' event
-                    google.maps.event.addListener( gmap, 'click', function(e) {
-                        marker.setPosition( e.latLng );
-                        setTimeout( function() { gmap.panTo( e.latLng ); }, 100 ); // just to smooth animation
-                        dispatch();
-                    });
-                    google.maps.event.addListener( gmap, 'zoom_changed', function() { dispatch(); });
-                    google.maps.event.addListener( marker, 'dragend', function() { dispatch() });
-
-                    function dispatch() {
-                        gmap_holder[0].dispatchEvent( new CustomEvent( 'map_changed', { detail: {
-                            'gmap' : gmap,
-                            'marker' : marker
-                        }}));
-                    }
-
-                    gmap_holder[0].addEventListener( 'map_changed', function(e) { // ++this goes to the form script.js
-                        // change the values immediately
-                        //console.log( e.detail.gmap );
-                        //marker.getPosition().lat();
-                        //marker.getPosition().long();
-                        //gmap.getZoom();
-                    });
-                    
-
-
-                },
-                //function() { fcAddGmapView( '.fct-gmap-view', true ) }, // ++restore this one to work as view
+                function() { fcAddGmapView( $gmap_holder, true ) },
                 ['google']
             );
         }
