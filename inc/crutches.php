dfc5976c20e07642765b4ba51678831da8d3e4b2
VVolkov833 <finnish.ru@gmail.com>
2023-02-13 20:44:19 +0100

    crutches for rank math seo

--- inc/crutches.php
+++ inc/crutches.php
@@ -18,3 +18,21 @@ add_action( 'transition_post_status', function($new_status, $old_status, $post) 
 // fix the gap, caused by wp-container-{ID}
 remove_filter( 'render_block', 'wp_render_layout_support_flag', 10, 2 );
 remove_filter( 'render_block', 'gutenberg_render_layout_support_flag', 10, 2 );
+
+
+// rankmath crutches
+add_filter( 'rank_math/sitemap/enable_caching', '__return_false' );
+
+// sitemap for doctors
+add_filter( 'rank_math/sitemap/post_type_archive_link', function( $archive_url, $post_type ){
+	return $post_type === 'doctor' ? false : $archive_url;
+}, 10, 2 );
+
+// rankmath breadcrumbs for doctors
+add_filter( 'rank_math/frontend/breadcrumb/items', function( $crumbs, $class ) {
+	if ( strpos( $crumbs[1][1], '/aerzte/' ) !== false ) {
+		$crumbs[1][0] = 'Kliniken';
+		$crumbs[1][1] = str_replace( '/aerzte/', '/kliniken/', $crumbs[1][1] );
+	}
+	return $crumbs;
+}, 10, 2);
